149751c37a63ebd19a423c371c03b9c6aae25a46
arjunlinksture <arjun@linksture.com>
2026-07-27 19:14:39 +0530

    Update archive-product-before.php

--- wp-content/themes/hongo/woocommerce/archive-product-before.php
+++ wp-content/themes/hongo/woocommerce/archive-product-before.php
@@ -22,8 +22,8 @@ $hongo_product_archive_layout_setting	= get_theme_mod( 'hongo_product_archive_la
 
 /* Filter for change layout style for ex. ?sidebar=right_sidebar */
 $hongo_product_archive_layout_setting	= apply_filters( 'hongo_page_layout_style', $hongo_product_archive_layout_setting );
-$hongo_product_sidebar_class			= ( $hongo_product_archive_container_style == 'container' ) ? ' col-lg-9 col-md-12 col-sm-12' :  ' col-lg-10 col-md-12 col-sm-12';
-$hongo_product_both_sidebar_class		= ( $hongo_product_archive_container_style == 'container' ) ? ' col-lg-6 col-md-12 col-sm-12' :  ' col-lg-8 col-md-12 col-sm-12';
+$hongo_product_sidebar_class			= ( $hongo_product_archive_container_style == 'container' ) ? ' col-xl-9 col-lg-12 col-md-12' :  ' col-xl-10 col-lg-12 col-md-12';
+$hongo_product_both_sidebar_class		= ( $hongo_product_archive_container_style == 'container' ) ? ' col-xl-6 col-lg-12 col-md-12' :  ' col-xl-8 col-lg-12 col-md-12';
 
 $hongo_product_archive_right_sidebar 	= get_theme_mod( 'hongo_product_archive_right_sidebar', 'hongo-shop-1' );
 $hongo_product_archive_left_sidebar 	= get_theme_mod( 'hongo_product_archive_left_sidebar', 'hongo-shop-1' );
@@ -85,26 +85,26 @@ if( $hongo_product_archive_layout_setting == 'hongo_layout_both_sidebar' ) {
 switch( $hongo_product_archive_layout_setting ) {
 	case 'hongo_layout_no_sidebar':
 		?>
-			<div class="col-sm-12 col-xs-12 hongo-content-full-part hongo-shop-content-part">
+			<div class="col-md-12 col-12 hongo-content-full-part hongo-shop-content-part">
 		<?php
 			do_action( 'hongo_shop_content_part_start' );
 		break;
 	case 'hongo_layout_left_sidebar':
 		?>
-			<div class="col-xs-12 pull-right hongo-content-right-part hongo-shop-content-part<?php echo esc_attr( $hongo_product_sidebar_class ); ?>">
+			<div class="col-12 pull-right hongo-content-right-part hongo-shop-content-part<?php echo esc_attr( $hongo_product_sidebar_class ); ?>">
 		<?php
 			do_action( 'hongo_shop_content_part_start' );
 		break;
 	case 'hongo_layout_right_sidebar':
 		?>
-			<div class="col-xs-12 hongo-content-left-part hongo-shop-content-part<?php echo esc_attr( $hongo_product_sidebar_class ); ?>">
+			<div class="col-12 hongo-content-left-part hongo-shop-content-part<?php echo esc_attr( $hongo_product_sidebar_class ); ?>">
 		<?php
 			do_action( 'hongo_shop_content_part_start' );
 		break;
 	case 'hongo_layout_both_sidebar':
 		?>
 			<div class="both-sidebar-wrap">
-				<div class="col-xs-12 both-content-center post-center hongo-content-center-part hongo-shop-content-part<?php echo esc_attr( $hongo_product_both_sidebar_class ); ?>">
+				<div class="col-12 both-content-center post-center hongo-content-center-part hongo-shop-content-part<?php echo esc_attr( $hongo_product_both_sidebar_class ); ?>">
 		<?php
 				do_action( 'hongo_shop_content_part_start' );
 		break;
