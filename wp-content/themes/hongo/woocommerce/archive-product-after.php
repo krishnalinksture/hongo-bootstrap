af5951f43355709af00451327a50728680d6616f
arjunlinksture <arjun@linksture.com>
2026-07-28 15:11:10 +0530

    Update archive-product-after.php

--- wp-content/themes/hongo/woocommerce/archive-product-after.php
+++ wp-content/themes/hongo/woocommerce/archive-product-after.php
@@ -93,7 +93,7 @@ switch ($hongo_product_archive_layout_setting) {
 		?>
 			</div>
 			<div class="hongo-product-common-sidebar-left-overlay"></div>
-			<div id="secondary" class="col-sm-4 col-xs-12 sidebar hongo-product-sidebar hongo-woocommerce-sidebar hongo-product-common-sidebar-left<?php echo esc_attr( $hongo_product_sidebar_class ); ?>" itemtype="http://schema.org/WPSideBar" itemscope="itemscope" role="complementary">
+			<div id="secondary" class="col-md-4 col-12 sidebar hongo-product-sidebar hongo-woocommerce-sidebar hongo-product-common-sidebar-left<?php echo esc_attr( $hongo_product_sidebar_class ); ?>" itemtype="http://schema.org/WPSideBar" itemscope="itemscope" role="complementary">
 				<a class="hongo-close-left-sidebar sidebar-close" href="javascript:void(0);">X</a>
 				<div class="hongo-product-common-sidebar-left-wrap">
 					<?php hongo_page_sidebar_style( $hongo_product_archive_left_sidebar ); ?>
@@ -106,7 +106,7 @@ switch ($hongo_product_archive_layout_setting) {
 		?>
 			</div>
 			<div class="hongo-product-common-sidebar-right-overlay"></div>
-			<div id="secondary" class="col-sm-4 col-xs-12 sidebar hongo-product-sidebar hongo-woocommerce-sidebar hongo-product-common-sidebar-right<?php echo esc_attr( $hongo_product_sidebar_class ); ?>" itemtype="http://schema.org/WPSideBar" itemscope="itemscope" role="complementary">
+			<div id="secondary" class="col-md-4 col-12 sidebar hongo-product-sidebar hongo-woocommerce-sidebar hongo-product-common-sidebar-right<?php echo esc_attr( $hongo_product_sidebar_class ); ?>" itemtype="http://schema.org/WPSideBar" itemscope="itemscope" role="complementary">
 				<a class="hongo-close-right-sidebar sidebar-close" href="javascript:void(0);">X</a>
 				<div class="hongo-product-common-sidebar-right-wrap">
 					<?php hongo_page_sidebar_style( $hongo_product_archive_right_sidebar ); ?>
@@ -119,14 +119,14 @@ switch ($hongo_product_archive_layout_setting) {
 		?>
 			</div>
 			<div class="hongo-product-common-sidebar-left-overlay"></div>
-			<div id="secondary" class="col-sm-6 col-xs-12 sidebar hongo-product-sidebar hongo-woocommerce-sidebar both-sidebar-left hongo-product-common-sidebar-left<?php echo esc_attr( $hongo_product_sidebar_class ); ?>" itemtype="http://schema.org/WPSideBar" itemscope="itemscope" role="complementary">
+			<div id="secondary" class="col-md-6 col-12 sidebar hongo-product-sidebar hongo-woocommerce-sidebar both-sidebar-left hongo-product-common-sidebar-left<?php echo esc_attr( $hongo_product_sidebar_class ); ?>" itemtype="http://schema.org/WPSideBar" itemscope="itemscope" role="complementary">
 				<a class="hongo-close-left-sidebar sidebar-close" href="javascript:void(0);">X</a>
 					<div class="hongo-product-common-sidebar-left-wrap">
 						<?php hongo_page_sidebar_style( $hongo_product_archive_left_sidebar ); ?>
 					</div>
 			</div>
 			<div class="hongo-product-common-sidebar-right-overlay"></div>
-			<div id="secondary" class="col-sm-6 col-xs-12 sidebar hongo-product-sidebar hongo-woocommerce-sidebar both-sidebar-right hongo-product-common-sidebar-right<?php echo esc_attr( $hongo_product_sidebar_class ); ?>" itemtype="http://schema.org/WPSideBar" itemscope="itemscope" role="complementary">
+			<div id="secondary" class="col-md-6 col-12 sidebar hongo-product-sidebar hongo-woocommerce-sidebar both-sidebar-right hongo-product-common-sidebar-right<?php echo esc_attr( $hongo_product_sidebar_class ); ?>" itemtype="http://schema.org/WPSideBar" itemscope="itemscope" role="complementary">
 				<a class="hongo-close-right-sidebar sidebar-close" href="javascript:void(0);">X</a>
 				<div class="hongo-product-common-sidebar-right-wrap">
 					<?php hongo_page_sidebar_style( $hongo_product_archive_right_sidebar ); ?>
