afacd68e8b48f7c016a554bbf49e334a552aa78b
piyushlinksture <piyush.ranipa@linksture.com>
2026-08-03 16:27:45 +0530

    Update archive-product.php

--- wp-content/themes/hongo/woocommerce/archive-product.php
+++ wp-content/themes/hongo/woocommerce/archive-product.php
@@ -42,66 +42,72 @@ get_header( 'shop' );
 				do_action( 'woocommerce_before_main_content' );
 
 				/* Check shop page */
-				wc_get_template_part( 'archive', 'product-before' );
-
-				/**
-				 * Hook: woocommerce_archive_description.
-				 *
-				 * @hooked woocommerce_taxonomy_archive_description - 10
-				 * @hooked woocommerce_product_archive_description - 10
-				 */
-				do_action( 'woocommerce_archive_description' );
-
-				if ( woocommerce_product_loop() ) {
+				?>
+				<div class="row">
+					<?php
+					wc_get_template_part( 'archive', 'product-before' );
 
 					/**
-					 * Hook: woocommerce_before_shop_loop.
+					 * Hook: woocommerce_archive_description.
 					 *
-					 * @hooked wc_print_notices - 10
-					 * @hooked woocommerce_result_count - 20
-					 * @hooked woocommerce_catalog_ordering - 30
+					 * @hooked woocommerce_taxonomy_archive_description - 10
+					 * @hooked woocommerce_product_archive_description - 10
 					 */
-					do_action( 'woocommerce_before_shop_loop' );
+					do_action( 'woocommerce_archive_description' );
 
-					woocommerce_product_loop_start();
+					if ( woocommerce_product_loop() ) {
 
-					if ( wc_get_loop_prop( 'total' ) ) {
-						while ( have_posts() ) {
-							the_post();
+						/**
+						 * Hook: woocommerce_before_shop_loop.
+						 *
+						 * @hooked wc_print_notices - 10
+						 * @hooked woocommerce_result_count - 20
+						 * @hooked woocommerce_catalog_ordering - 30
+						 */
+						do_action( 'woocommerce_before_shop_loop' );
 
-							/**
-							 * Hook: woocommerce_shop_loop.
-							 *
-							 * @hooked WC_Structured_Data::generate_product_data() - 10
-							 */
-							do_action( 'woocommerce_shop_loop' );
+						woocommerce_product_loop_start();
 
-							wc_get_template_part( 'content', 'product' );
+						if ( wc_get_loop_prop( 'total' ) ) {
+							while ( have_posts() ) {
+								the_post();
+
+								/**
+								 * Hook: woocommerce_shop_loop.
+								 *
+								 * @hooked WC_Structured_Data::generate_product_data() - 10
+								 */
+								do_action( 'woocommerce_shop_loop' );
+
+								wc_get_template_part( 'content', 'product' );
+							}
 						}
+
+						woocommerce_product_loop_end();
+
+						/**
+						 * Hook: woocommerce_after_shop_loop.
+						 *
+						 * @hooked woocommerce_pagination - 10
+						 */
+						do_action( 'woocommerce_after_shop_loop' );
+
+					} else {
+
+						/**
+						 * Hook: woocommerce_no_products_found.
+						 *
+						 * @hooked wc_no_products_found - 10
+						 */
+
+						do_action( 'woocommerce_no_products_found' );
 					}
 
-					woocommerce_product_loop_end();
-
-					/**
-					 * Hook: woocommerce_after_shop_loop.
-					 *
-					 * @hooked woocommerce_pagination - 10
-					 */
-					do_action( 'woocommerce_after_shop_loop' );
-
-				} else {
-
-					/**
-					 * Hook: woocommerce_no_products_found.
-					 *
-					 * @hooked wc_no_products_found - 10
-					 */
-					
-					do_action( 'woocommerce_no_products_found' );
-				}
-
-				/* Check shop page */
-				wc_get_template_part( 'archive', 'product-after' );
+					/* Check shop page */
+					wc_get_template_part( 'archive', 'product-after' );
+					?>
+				</div>
+				<?php
 
 				/**
 				 * Hook: woocommerce_after_main_content.
@@ -116,7 +122,7 @@ get_header( 'shop' );
 				 * @hooked woocommerce_get_sidebar - 10
 				 */
 				do_action( 'woocommerce_sidebar' );
-			?>
+				?>
 			</div>
 		</div>
 	</section>
