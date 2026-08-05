e43252fb02435781ea83ee91e92a6a9aee68b5be
HIren Linksture <hiren@linksture.com>
2026-08-05 17:11:52 +0530

    blog

--- wp-content/themes/hongo/lib/hongo-extra-functions.php
+++ wp-content/themes/hongo/lib/hongo-extra-functions.php
@@ -1021,7 +1021,7 @@ if ( ! function_exists( 'hongo_related_posts' ) ) {
                     echo '<span class="related-post-general-title alt-font">'.esc_html( $hongo_related_posts_title ).'</span>';
                 echo '</div>';
             }
-            echo '<ul class="post-grid">';
+            echo '<ul class="post-grid row">';
                 echo '<li class="grid-sizer"></li>';
                 while ( $recent_post->have_posts() ) {
                     
