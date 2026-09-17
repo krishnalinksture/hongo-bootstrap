69e7d4a2b69d10e9ffdc3ba0784683126c8ba802
piyushlinksture <piyush.ranipa@linksture.com>
2026-09-17 18:59:21 +0530

    Column Offset and sm width issue

--- wp-content/plugins/hongo-addons/hongo-shortcodes/shortcodes/hongo-shortcode-column.php
+++ wp-content/plugins/hongo-addons/hongo-shortcodes/shortcodes/hongo-shortcode-column.php
@@ -100,11 +100,6 @@ if ( ! function_exists( 'hongo_column' ) ) {
 		! empty( $desktop_height ) ? $style_array[] = "min-height: ".$desktop_height.";" : '';
 
 		// Column Offset and sm width
-		// strchr( $offset, 'col-xs' ) ? '' : $classes[] = "col-xs-mobile-fullwidth";
-		// $width = wpb_translateColumnWidthToSpan( $width );
-		// $classes[] = $width = vc_column_offset_class_merge( $offset, $width );
-
-		// Column Offset and sm width
 		strchr( $offset, 'col-xs' ) ? '' : $classes[] = "col-xs-mobile-fullwidth";
 
 		$width = wpb_translateColumnWidthToSpan( $width );
